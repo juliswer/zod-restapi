d61acc1624cef1c8dae01dd86759498eb64e356e
juliswer <julianswer36@gmail.com>
2022-04-30 11:09:40 -0300

    routes and controllers

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,3 +1,23 @@
+import { z, ZodError } from "zod";
+
+const loginSchema = z.object({
+  email: z.string().email().nonempty("Email is required"),
+  password: z.string().nonempty("Password is required"),
+});
+
 export const postUser = (req: any, res: any) => {
-  res.send("hello world");
+  try {
+    loginSchema.parse(req.body);
+
+    res.send("hello world");
+  } catch (error) {
+    if (error instanceof ZodError) {
+      return res.status(400).json(
+        error.issues.map((issue) => ({
+          message: issue.message,
+        }))
+      );
+    }
+    return res.statsu(500).json({ message: "Intenal server error" }); 
+  }
 };
