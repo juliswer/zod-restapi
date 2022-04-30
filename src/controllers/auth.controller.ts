52b7285b25939396c90fa962c9eed3a06419561d
juliswer <julianswer36@gmail.com>
2022-04-30 11:15:29 -0300

    routes and controllers

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -2,15 +2,17 @@ import { z, ZodError } from "zod";
 
 const loginSchema = z.object({
   email: z.string().nonempty("Email is required").email({
-    message: "Email is invalid"
+    message: "Email is invalid",
   }),
-  password: z.string().nonempty("Password is required"),
+  password: z.string().nonempty("Password is required").min(6, {
+    message: "Password must be at least 6 characters",
+  }),
 });
 
 export const postUser = (req: any, res: any) => {
   try {
     const schemaRes = loginSchema.parse(req.body);
-    console.log(schemaRes)
+    console.log(schemaRes);
 
     res.send("login");
   } catch (error) {
@@ -21,6 +23,6 @@ export const postUser = (req: any, res: any) => {
         }))
       );
     }
-    return res.statsu(500).json({ message: "Intenal server error" }); 
+    return res.statsu(500).json({ message: "Intenal server error" });
   }
 };
