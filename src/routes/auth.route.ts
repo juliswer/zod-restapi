282ce21256f3329749399e8eed1f7d9b14831078
juliswer <julianswer36@gmail.com>
2022-04-30 18:49:37 -0300

    middleware setting

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,8 +1,10 @@
-import {Router} from 'express';
-import {postUser} from '../controllers/auth.controller'
+import { Router } from "express";
+import { postUser } from "../controllers/auth.controller";
+import { schemaValidation } from "../middlewares/schemaValidator.middleware";
+import { loginSchema } from "../schemas/auth.schema";
 
 const router = Router();
 
-router.post("/login", postUser)
+router.post("/login", schemaValidation(loginSchema), postUser);
 
 export default router;
