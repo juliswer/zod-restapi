f646732274d2fab99484715234b7e74b6ede0ceb
juliswer <julianswer36@gmail.com>
2022-04-30 13:06:43 -0300

    more routes

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import authRoutes from "./routes/auth.route";
+import productRoutes from "./routes/product.route";
 
 const app = express();
 
 app.use(express.json());
 
 app.use(authRoutes);
+app.use(productRoutes);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
